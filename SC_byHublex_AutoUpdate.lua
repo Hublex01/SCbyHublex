27a88a8d2a0d24d30752da7b163822ee9db0c04a
habib-nematulloev <muclasis@gmail.com>
2022-03-04 02:15:24 +1000

    test to correct at. 2

--- SC_byHublex_AutoUpdate.lua
+++ SC_byHublex_AutoUpdate.lua
@@ -1,13 +1,18 @@
-function loadsource()
-  API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
-  if not API then
-    gg.toast('failed to load source!')
-  else
-    local vfa, vfb = pcall(load(API))
-    if not vfa then
-      print(vfb)
-    end
-  end
+API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
+if not API then
+  gg.alert('Error')
+else
+  pcall(load(API))
 end
-
-loadsource()
+--function loadsource()
+--  if not API then
+--    gg.toast('failed to load source!')
+--  else
+--    local vfa, vfb = pcall(load(API))
+--    if not vfa then
+--      print(vfb)
+--    end
+--  end
+--end
+--API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
+--loadsource()
