7e2de6d35a2931fff043f55b7e7496a91ca2f136
habib-nematulloev <muclasis@gmail.com>
2022-03-04 10:18:45 +1000

    edit

--- SC_byHublex_AutoUpdate.lua
+++ SC_byHublex_AutoUpdate.lua
@@ -1,4 +1,5 @@
-API = gg.makeRequest('https://pastebin.com/raw/HM4EMcWs').content
+--API = gg.makeRequest('https://pastebin.com/raw/HM4EMcWs').content
+API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
 if not API then
   gg.alert('Error')
 else
