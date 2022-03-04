01d2fe3284bbe3ec1cefaa2ca2c6c3bd5e56e140
habib-nematulloev <78960886+habib-nematulloev@users.noreply.github.com>
2022-03-04 10:26:47 +1000

    Update SC_byHublex_AutoUpdate.lua

--- SC_byHublex_AutoUpdate.lua
+++ SC_byHublex_AutoUpdate.lua
@@ -1,10 +1,16 @@
 --API = gg.makeRequest('https://pastebin.com/raw/HM4EMcWs').content
+gg.toast('Start')
 API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
+gg.toast('Start 1')
 if not API then
   gg.alert('Error')
+  gg.toast('Start 2')
 else
+  gg.toast('Start 3')
   pcall(load(API))
+  gg.toast('Start 4')
 end
+gg.toast('Start 5')
 --function loadsource()
 --  if not API then
 --    gg.toast('failed to load source!')
