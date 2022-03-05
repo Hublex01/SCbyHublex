0c29028c3657c568c8bac189d81b3ce7a957edac
habib-nematulloev <78960886+habib-nematulloev@users.noreply.github.com>
2022-03-05 21:08:27 +1000

    Update SC_byHublex_AutoUpdate.lua

--- SC_byHublex_AutoUpdate.lua
+++ SC_byHublex_AutoUpdate.lua
@@ -1,25 +1,21 @@
 --API = gg.makeRequest('https://pastebin.com/raw/HM4EMcWs').content
-gg.toast('Start')
 API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
-gg.toast('Start 1')
 if not API then
-  gg.alert('Error')
-  gg.toast('Start 2')
+  gg.toast('oh no')
 else
-  gg.toast('Start 3')
+  gg.toast('github loaded')
   pcall(load(API))
-  gg.toast('Start 4')
 end
-gg.toast('Start 5')
---function loadsource()
---  if not API then
---    gg.toast('failed to load source!')
---  else
---    local vfa, vfb = pcall(load(API))
---    if not vfa then
---      print(vfb)
---    end
---  end
---end
---API = gg.makeRequest('https://raw.githubusercontent.com/habib-nematulloev/SC_byHublex/main/SC_byHublex.lua').content
---loadsource()
+function loadsource()
+  if not API then
+    gg.toast('failed to load source!')
+  else
+    gg.toast('pastebin loaded')
+    local vfa, vfb = pcall(load(API))
+    if not vfa then
+      print(vfb)
+    end
+  end
+end
+API = gg.makeRequest('https://pastebin.com/raw/HM4EMcWs').content
+loadsource()
