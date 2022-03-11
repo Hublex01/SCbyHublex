76f2fd873d03d0c1747c0dda1d4411671572cafb
habib-nematulloev <muclasis@gmail.com>
2022-03-11 10:56:10 +1000

    Edit code

--- SCbyHublex.lua
+++ SCbyHublex.lua
@@ -1,4 +1,4 @@
-gg.toast('test hahha')
+gg.toast('haha')
 ---------------------
 -----SC_byHublex-----
 ---------------------
@@ -22,14 +22,15 @@ function main()
   --Character Glow--
   elseif menu == 2 then
     CharacterGlow()
-
+  
+  
   --No select--
   else
   NoSelect()
   end
 
 end
-  
+
 -----Infinity Fly-----
 function InfinityFly()
   gg.setRanges(gg.REGION_CODE_APP)
@@ -40,14 +41,12 @@ function InfinityFly()
     gg.editAll("0.95",gg.TYPE_FLOAT)
     statusInfinityFly = on
     gg.toast('Infinity Fly is ON')
-  elseif statusInfinityFly == on then
+  else
     gg.searchNumber("0.95",gg.TYPE_FLOAT)
     gg.getResults(1000)
     gg.editAll("0.80",gg.TYPE_FLOAT)
     statusInfinityFly = off
     gg.toast('Infinity Fly is OFF')
-  else
-    gg.toast('ERROR')
   end
   gg.clearResults()
 end       
@@ -62,13 +61,11 @@ function CharacterGlow()
     gg.setVisible(false)
     gg.getResults(1000)
     gg.editAll("100", gg.TYPE_FLOAT)
-  elseif statusCharacterGlow = on then
+  else
     gg.searchNumber("100",gg.TYPE_FLOAT)
     gg.setVisible(false)
     gg.getResults(1000)
     gg.editAll("0.9", gg.TYPE_FLOAT)
-  else
-    gg.toast('ERROR')
   end
   gg.clearResults()
 end
