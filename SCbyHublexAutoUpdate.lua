046f360cc76146d6452e94de8a1cd1f522d37d71
habib-nematulloev <muclasis@gmail.com>
2022-03-13 09:53:32 +1000

    Delete log

--- SCbyHublexAutoUpdate.lua
+++ SCbyHublexAutoUpdate.lua
@@ -2,7 +2,6 @@
 --If you can upgrade my script, write me in discord Hublex#2923.
 --Repository: https://github.com/Hublex01/SCbyHublex
 API = gg.makeRequest('https://raw.githubusercontent.com/Hublex01/SCbyHublex/main/SCbyHublex.lua').content
-print(API)
 if not API then
   gg.toast('No connection or repository is deleted')
 else
